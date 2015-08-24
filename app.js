3cd0ee3ca000bdcca15b0721d3dfb6ee513ab0e6
DAN YU <danyu1996@gmail.com>
2015-08-23 22:00:53 -0400

    adding basic user authentication now

--- app.js
+++ app.js
@@ -98,15 +98,8 @@ app.post('/emailtest', indexController.getemailtest);
 /**
  * Primary app routes: Homepage, and rest of application.
  */
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
+app.get('/home', homeController.index);
 app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
